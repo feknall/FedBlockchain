7949553735964442d38a340e4b283596bdba3b8c
feknall <h.fazli.k@gmail.com>
2022-11-02 20:35:30 -0400

    Networking problem is fixed. Client, issuer, and verifier can be run using docker-compose run command.

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 docker build -t dist-fed-core-fl . -f fl/Dockerfile
+
+docker build -t dist-fed-core-identity . -f identity/Dockerfile
+docker build -t dist-fed-core-identity-client . -f identity/client/Dockerfile
+docker build -t dist-fed-core-identity-issuer . -f identity/issuer/Dockerfile
+docker build -t dist-fed-core-identity-verifier . -f identity/verifier/Dockerfile
