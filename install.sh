980c6467e0e6fa63166245d071646244a99d8c92
feknall <h.fazli.k@gmail.com>
2022-11-03 11:06:58 -0400

    Use global docker image

--- install.sh
+++ install.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 docker build -t dist-fed-core-fl . -f fl/Dockerfile
+docker tag dist-fed-core-fl hmaid/hyperledger:dist-fed-core-fl
+docker push hmaid/hyperledger:dist-fed-core-fl
 
 docker build -t dist-fed-core-identity . -f identity/Dockerfile
+docker tag dist-fed-core-identity hmaid/hyperledger:dist-fed-core-identity
+docker push hmaid/hyperledger:dist-fed-core-identity
+
 docker build -t dist-fed-core-identity-client . -f identity/client/Dockerfile
+docker tag dist-fed-core-identity-client hmaid/hyperledger:dist-fed-core-identity-client
+docker push hmaid/hyperledger:dist-fed-core-identity-client
+
 docker build -t dist-fed-core-identity-issuer . -f identity/issuer/Dockerfile
+docker tag dist-fed-core-identity-issuer hmaid/hyperledger:dist-fed-core-identity-issuer
+docker push hmaid/hyperledger:dist-fed-core-identity-issuer
+
 docker build -t dist-fed-core-identity-verifier . -f identity/verifier/Dockerfile
+docker tag dist-fed-core-identity-verifier hmaid/hyperledger:dist-fed-core-identity-verifier
+docker push hmaid/hyperledger:dist-fed-core-identity-verifier
